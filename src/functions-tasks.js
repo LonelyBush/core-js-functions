56843c31d3bcec6d6f75f975b283a80e779c16af
LonelyBush <lonely.bush68@gmail.com>
2024-04-09 19:57:11 +0300

    feat: done partialUsingArguments and getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -207,8 +207,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function n(...args2) {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
@@ -228,8 +230,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let result = startFrom;
+  return function getIdGenerator() {
+    result += 1;
+    return result - 1;
+  };
 }
 
 module.exports = {
